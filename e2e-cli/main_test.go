3644d70a2b6114b0debd9cac6bbb592db8f68bfd
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-09-11 18:21:19 +0200

    feat: update controller versions (#90)

--- e2e-cli/main_test.go
+++ e2e-cli/main_test.go
@@ -69,14 +69,15 @@ func retrieveBootStrapConfigVars() (string, string, error) {
 func bootstrapGithub(owner, token string) (*bootstrap.GithubCmd, error) {
 	ctx := context.Background()
 	bootstrapGithubCmd := bootstrap.GithubCmd{
-		Owner:              owner,
-		Repository:         repository,
-		Token:              token,
-		Path:               targetPath,
-		Registry:           registry,
-		Components:         env.Components,
-		DockerconfigPath:   cfg.DockerconfigPath,
-		DestructiveActions: true,
+		Owner:                         owner,
+		Repository:                    repository,
+		Token:                         token,
+		Path:                          targetPath,
+		Registry:                      registry,
+		Components:                    env.Components,
+		DockerconfigPath:              cfg.DockerconfigPath,
+		DestructiveActions:            true,
+		GenerateSelfSignedCertificate: true,
 	}
 
 	// set kubeconfig
@@ -107,17 +108,18 @@ func bootstrapGithub(owner, token string) (*bootstrap.GithubCmd, error) {
 func bootstrapGitea(owner, token, hostname string) (*bootstrap.GiteaCmd, error) {
 	ctx := context.Background()
 	bootstrapGiteaCmd := bootstrap.GiteaCmd{
-		Owner:              owner,
-		Repository:         repository,
-		Token:              token,
-		Hostname:           hostname,
-		Personal:           true,
-		Path:               targetPath,
-		Registry:           registry,
-		TestURL:            fmt.Sprintf("http://%s/%s/%s", defautHostname, owner, repository),
-		Components:         env.Components,
-		DockerconfigPath:   cfg.DockerconfigPath,
-		DestructiveActions: true,
+		Owner:                         owner,
+		Repository:                    repository,
+		Token:                         token,
+		Hostname:                      hostname,
+		Personal:                      true,
+		Path:                          targetPath,
+		Registry:                      registry,
+		TestURL:                       fmt.Sprintf("http://%s/%s/%s", defautHostname, owner, repository),
+		Components:                    env.Components,
+		DockerconfigPath:              cfg.DockerconfigPath,
+		DestructiveActions:            true,
+		GenerateSelfSignedCertificate: true,
 	}
 
 	// set kubeconfig
